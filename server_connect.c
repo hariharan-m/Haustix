c0e743a4d094500ba70156d35ccb0623c5fa2cdf
Hariharan <hariharan.mathavan@gmail.com>
2017-12-10 06:29:34 -0500

    more changes

--- server_connect.c
+++ server_connect.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/socket.h>
+#include <arpa/inet.h> 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static char bt_out_str[1024];
 static char bt_in_str[1024];
@@ -26,25 +33,25 @@ int main(int argc, char **argv)
 {
     struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
     memset(bt_in_str,0,sizeof(bt_in_str));
-    int s, client, bytes_read,disc_flag=0; //To Disconnect
+    int btsocket, btclient, bytes_read,disc_flag=0; //To Disconnect
     socklen_t opt = sizeof(rem_addr);
 
     // allocate socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    btsocket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
     // bind socket to port 1 of the first available 
     // local bluetooth adapter
     loc_addr.rc_family = AF_BLUETOOTH;
     loc_addr.rc_bdaddr = *BDADDR_ANY;
     loc_addr.rc_channel = (uint8_t) 1;
-    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+    bind(btsocket, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 while(1){  //Will re-enter when disconnected
 
     // put socket into listening mode
-    listen(s, 1);
+    listen(btsocket, 1);
 
     // accept one connection NOTE:BLOCKING
-    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+    btclient = accept(btsocket, (struct sockaddr *)&rem_addr, &opt);
     ba2str( &rem_addr.rc_bdaddr, bt_in_str );
 
     printf("accepted connection from %s\n", bt_in_str);
@@ -53,7 +60,8 @@ while(1){  //Will re-enter when disconnected
     {
     // read data from the client
         memset(bt_in_str, 0, sizeof(bt_in_str));
-        bytes_read = read(client, bt_in_str, sizeof(bt_in_str));
+        bytes_read = read(btclient, bt_in_str, sizeof(bt_in_str)-1);
+        bt_in_str[bytes_read]=0;
         if( bytes_read > 0 ) {
             printf("received [%s]\n", bt_in_str);
             if(bt_in_str[0]=='0')
@@ -63,9 +71,9 @@ while(1){  //Will re-enter when disconnected
         }
     }
     // close connection
-    close(client);
+    close(btclient);
     }
-    close(s);
+    close(btsocket);
     return 0;
 }
 
